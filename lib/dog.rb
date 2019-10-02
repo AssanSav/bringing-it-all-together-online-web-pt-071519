69f83ce92879dfb285a1470bf78e0ae7f07d013b
assane savadogo <assane.savadogo81@gmail.com>
2019-10-02 02:54:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, AND breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       row = dog[0]
       Dog.new(id: row[0], name: row[1], breed: row[2])
