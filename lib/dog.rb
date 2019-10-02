dc3f78c73efa6160b8becd8aa38de5bab33d3511
assane savadogo <assane.savadogo81@gmail.com>
2019-10-02 02:21:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,7 +20,7 @@ class Dog
   end
   
   def self.find_by_name(name)
-    r = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name).
+    r = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name).first
     binding.pry
   end
   
