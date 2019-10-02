de2fbb2d37a45551b9c72422f8d84dd3461e7b45
assane savadogo <assane.savadogo81@gmail.com>
2019-10-02 02:17:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,10 +20,9 @@ class Dog
   end
   
   def self.find_by_name(name)
-    sql = ("SELECT * FROM dogs WHERE name = ?")
+    sql = "SELECT * FROM dogs WHERE name = ?"
     binding.pry
     r = DB[:conn].execute(sql, name).map do |row|
-    
     self.new_from_db(row)
     end
   end
